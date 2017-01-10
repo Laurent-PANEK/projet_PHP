5a6c2103ea0d31b8b52edc60875262dfbf885ae0
Laurent-PANEK <laurent.panek@ynov.com>
2017-01-10 11:50:02 +0100

    refresh

--- connect.php
+++ connect.php
@@ -1,5 +1,7 @@
 <?php
 session_start();
+ini_set('display_errors',1);
+ini_set('error_reporting',E_ALL);
 /* Connexion à une base ODBC avec l'invocation de pilote */
 $dsn = 'mysql:dbname=projet_php;host=127.0.0.1';
 $user = 'root';
