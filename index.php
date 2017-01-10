33625873a47cbf13e9b77b87b0cae708472344c3
Abdessalam98 <belmehelabdessalam.benharira@ynov.com>
2017-01-10 14:52:46 +0100

    addFiles

--- index.php
+++ index.php
@@ -1,58 +1,48 @@
 <?PHP
-require 'connect.php';
+require('db_connexion.php');
 
-function get_ip()
-{
-    // IP si internet partagé
-    if (isset($_SERVER['HTTP_CLIENT_IP'])) {
-        return $_SERVER['HTTP_CLIENT_IP'];
-    } // IP derrière un proxy
-    elseif (isset($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        return $_SERVER['HTTP_X_FORWARDED_FOR'];
-    } // Sinon : IP normale
-    else {
-        return (isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '');
-    }
-}
 
-echo 'Adresse IP du visiteur : ' . get_ip();
 
 ?>
 
 
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
-    <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
-    <link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-    <link rel="stylesheet" href="css/style_index.css"> <!-- Resource style -->
-    <script src="js/modernizr.js"></script> <!-- Modernizr -->
-    <title>HostPics | hébergement gratuit d'images</title>
-</head>
-<body>
-<header>
-    <div class="cd-logo"><a href="#0"><img src="img/cd-logo.svg" alt="Logo"></a></div>
+        <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+        <link rel='stylesheet prefetch' href='https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css'>
+        <link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
+        <link rel="stylesheet" href="css/style_index.css"> <!-- Resource style -->
+        <script src="js/modernizr.js"></script> <!-- Modernizr -->
+        <title>HostPics | hébergement gratuit d'images</title>
+    </head>
+    <body>
+        <header>
+            <div class="cd-logo"><a href="#0"><img src="img/cd-logo.svg" alt="Logo"></a></div>
 
-    <nav class="cd-main-nav-wrapper">
-        <ul class="cd-main-nav">
-            <li><a href="inscription.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Inscription</a></li>
-            <li><a href="connexion.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Connexion</a></li>
-            <li><a href="#0"><i class="fa fa-question" aria-hidden="true"></i> A Propos</a></li>
-            <li><a href="#0"><i class="fa fa-phone" aria-hidden="true"></i> Nous Contacter</a></li>
-        </ul> <!-- .cd-main-nav -->
-    </nav> <!-- .cd-main-nav-wrapper -->
+            <nav class="cd-main-nav-wrapper">
+                <ul class="cd-main-nav">
+                    <li><a href="inscription.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Inscription</a></li>
+                    <li><a href="connexion.php"><i class="fa fa-sign-in" aria-hidden="true"></i> Connexion</a></li>
+                    <li><a href="#0"><i class="fa fa-question" aria-hidden="true"></i> A Propos</a></li>
+                    <li><a href="#0"><i class="fa fa-phone" aria-hidden="true"></i> Nous Contacter</a></li>
+                </ul> <!-- .cd-main-nav -->
+            </nav> <!-- .cd-main-nav-wrapper -->
 
-    <a href="#0" class="cd-nav-trigger">Menu<span></span></a>
-</header>
-
-<main class="cd-main-content">
-    <!-- main content here -->
-</main>
-<script src="js/jquery-2.1.1.js"></script>
-<script src="js/main.js"></script> <!-- Resource jQuery -->
-</body>
+            <a href="#0" class="cd-nav-trigger">Menu<span></span></a>
+        </header>
+            <div class="cd-main-content">
+                <!-- main content here -->
+            </div>
+        <script src="js/jquery-2.1.1.js"></script>
+        <script src="js/main.js"></script> <!-- Resource jQuery -->
+    </body>
 </html>
