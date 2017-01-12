ba7e83187ff22c4e1b317e5d1b74c460ed62997d
abdeldouhi <abdessamadov@hotmail.fr>
2017-01-12 10:48:25 +0100

    upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,4 @@
-<?php
+²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²²<?php
 require 'function.php';
 $req = new SQLrequete('root', '', 'projet_php');
 $req->upload();
@@ -12,19 +12,33 @@ $req->upload();
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
     <link rel="stylesheet" href="css/style_index.css"> <!-- Resource style -->
+    <link rel="stylesheet" href="css/animate.css"> <!--animation-->
+    <link rel="stylesheet" href="css/upload.css">
+
     <title>Document</title>
 </head>
 <body>
 <?php include('header.php'); ?>
 <div class="cd-main-content">
-    <br><br><br><br><br>
-<form action="" enctype="multipart/form-data" method="post">
-    <input type="file" name="nomDuInput" id="file">
-    <input type="text" name="title" id="title" placeholder="title">
-    <button type="submit">Envoyer</button>
+    <div class="all" style="background-color: dimgrey ; margin: 10vh 20vw 0 20vw; min-width: 250px; min-height: 360px;">
+        <br><br><br>
+<form  action="" enctype="multipart/form-data" method="post">
+
+      <div class="imgcenter">  <img  src="img/upload.png" width="150px" height="120px" alt="" class="myAvatar" style="position:; top:0; left:0; z-index:1;min-height: 120px;min-width: 150px" >
+        <input type="file" name="newAvatar" id="newAvatar" style="width:100%x; height:150px; position:relative; top:0px; left:0; z-index:2; opacity:0;">
+
+<div class="hover1">
+    <input class="title" type="text" name="title"  placeholder="TITLE">
+
+    <br>
+
+    <button class="button" style="vertical-align:middle"><span>Hover </span>
+</div>
+
 </form>
+    </div>
 </div>
 <script src="js/jquery-2.1.1.js"></script>
 <script src="js/main.js"></script> <!--Resource jQuery-->
