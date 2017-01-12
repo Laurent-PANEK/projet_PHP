ac9906dd26f3b242b875fd340f4a1b37003943c3
abdeldouhi <abdessamadov@hotmail.fr>
2017-01-12 16:30:28 +0100

    update

--- view.php
+++ view.php
@@ -14,8 +14,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="css/reset.css"> <!-- CSS reset -->
-    <link rel="stylesheet" href="css/style_index.css"> <!-- Resource style -->
+
+
     <link rel="stylesheet" href="css/animate.css"> <!--animation-->
     <link rel="stylesheet" href="css/view.css">
 
@@ -26,19 +26,18 @@
 <div class="cd-main-content">
 <div class="all1">
     <img class="imgsrc" src="http://www.codeproject.com/KB/GDI-plus/ImageProcessing2/img.jpg" alt="error">
-   <div class="from">
+   <div class="form">
     <form class="links" action="">
-        <?php  echo 'code <\HTML> : '   ?>
+       <p class="html1">  code <\HTML> : </p>
+        <input class="input" type="text" name="" id="">
         <br>
-        <input type="text" name="" id="">
+        <p class="html1">  code <\HTML> : </p>
+
+        <input class="input" type="text" name="" id="">
         <br>
-        <?php  echo 'code <\HTML> : '   ?>
-        <br>
-        <input type="text" name="" id="">
-        <br>
-        <?php  echo 'code <\HTML> : '   ?>
-        <br>
-        <input type="text" name="" id="">
+        <p class="html1">  code <\HTML> : </p>
+
+        <input class="input" type="text" name="" id="">
         <br>
     </form>
    </div>
